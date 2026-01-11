2cd1161f367845a34f453bde20e907a51f30214c
difarizkianaf <difarizkianaf@gmail.com>
2026-01-11 22:27:23 +0700

    week10-pattern-testing: implement singleton, mvc, and junit test

---
+++ praktikum/week10-pattern-testing/src/main/java/com/upb/agripos/AppMVC.java
@@ -0,0 +1,20 @@
+package com.upb.agripos;
+
+import com.upb.agripos.config.DatabaseConnection;
+import com.upb.agripos.controller.ProductController;
+import com.upb.agripos.model.Product;
+import com.upb.agripos.view.ConsoleView;
+
+public class AppMVC {
+    public static void main(String[] args) {
+        System.out.println("Hello, Difa Rizkiana Fauziyah - 240320564 (Week-10 Pattern Testing)");
+        DatabaseConnection db1 = DatabaseConnection.getInstance();
+        db1.connect();
+
+
+        Product product = new Product("P01", "Pupuk Organik");
+        ConsoleView view = new ConsoleView();
+        ProductController controller = new ProductController(product, view);
+        controller.showProduct();
+    }
+}
